09fe6c47c78ea8cb0238cc39aebc9541847126c2
riyazd19 <91775182+riyazd19@users.noreply.github.com>
2024-02-24 13:04:41 +0530

    first push

---
+++ Mailosaur_API_24-02-2024/src/main/java/mailTestingFolderOnePackage/TestPush.java
@@ -0,0 +1,11 @@
+package mailTestingFolderOnePackage;
+
+public class TestPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mail testing new push");
+
+	}
+
+}
